85302de89969fa2663f61d55026ee454ed4577fb
Emiliano <v-epollero@expedia.com>
2017-01-06 15:17:34 -0300

    second commit

--- src/automationFramework/NavigateCommands.java
+++ src/automationFramework/NavigateCommands.java
@@ -1,10 +1,21 @@
 package automationFramework;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class NavigateCommands {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		WebDriver driver = new FirefoxDriver();
+		String appUrl = "http://www.DemoQA.com";
+		driver.get(appUrl);
+		driver.findElement(By.xpath(".//*[@id='menu-item-374']/a")).click();
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().to(appUrl);
+		driver.navigate().refresh();
+		driver.close();
 	}
 
 }
